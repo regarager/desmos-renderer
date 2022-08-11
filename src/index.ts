8b4352c0a36900c2672bfea5c87571c01f438b80
BuildTools <unconfigured@null.spigotmc.org>
2022-08-10 22:54:06 -0700

    added desmos api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import yargs from "yargs/yargs";
 
 const server = express();
@@ -7,6 +8,13 @@ const argv = yargs(process.argv.slice(2)).options({
     dir: { type: "string", demandOption: true },
 }).parseSync();
 
+if (!argv.dir.startsWith("/")) argv.dir = path.join(path.join(__dirname, "../"), argv.dir);
+
+server.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 server.listen(port, () => {
     console.log("Server started");
+    console.log(`Input directory: ${argv.dir}`);
 });
